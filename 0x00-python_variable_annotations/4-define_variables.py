cf814a78307058cb3280006bc80ddd421e48117a
Yohannes251 <yohannes.z251@gmail.com>
2022-04-14 20:13:10 +0300

    Variables with type annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Contains type conversion function with type annotation
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
