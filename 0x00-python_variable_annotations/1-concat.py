0e6944b5ae2a85a96db1f19ee50eaeb0649570e4
Yohannes251 <yohannes.z251@gmail.com>
2022-04-14 16:55:24 +0300

    String concat with type annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Contains string concatenation function with type annotation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns concatenated string from arguments"""
+    return str1 + str2
