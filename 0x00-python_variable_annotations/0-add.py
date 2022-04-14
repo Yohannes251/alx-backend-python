46e587b1183070b5f507be3614b682839e83d8ef
Yohannes251 <yohannes.z251@gmail.com>
2022-04-14 16:51:07 +0300

    First task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Contains addition function with type annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Returns sum of arguments"""
+    return a + b
