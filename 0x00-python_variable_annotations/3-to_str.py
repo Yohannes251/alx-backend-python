f5e161c3c5c1424519e13753b45c1e355908991b
Yohannes251 <yohannes.z251@gmail.com>
2022-04-14 20:07:28 +0300

    Float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Contains type conversion function with type annotation
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float number"""
+    return str(n)
