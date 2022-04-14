b25a5b2e7db74239a9d411d1d991eeef5061318e
Yohannes251 <yohannes.z251@gmail.com>
2022-04-14 20:02:33 +0300

    Floor of float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Contains type conversion function with type annotation
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns floor of a float number"""
+    return math.floor(n)
